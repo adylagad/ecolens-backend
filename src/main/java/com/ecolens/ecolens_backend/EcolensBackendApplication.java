9fd0bcdd606100b422076a05c4e47263851a08cf
adylagad <alagad@usc.edu>
2026-02-11 13:06:47 -0800

    Initialize Spring Boot project scaffold

---
+++ src/main/java/com/ecolens/ecolens_backend/EcolensBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecolens.ecolens_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcolensBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcolensBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecolens/ecolens_backend/EcolensBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecolens.ecolens_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcolensBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
